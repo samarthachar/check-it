f3f2ab8c0974a443fff0c05d028c827d7ad947a5
Samarth <achar.samarth@gmail.com>
2026-01-22 19:24:36 +0000

    Changes Made: Removed redundant code

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, request, url_for, redirect, jsonify, flash
+from flask import Flask, render_template, abort, request, url_for, redirect, flash
 from flask_bootstrap import Bootstrap
 import secrets
 from flask import Flask, render_template,redirect, url_for
